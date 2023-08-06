12b1d1bc6b4db43f42ae61dcecb2eb259ab269f7
ayaya <ayaya6436@gmail.com>
2023-08-06 01:55:35 +0000

    update et fin Depenses

--- src/main/java/apiBudget/apiBudget/controller/advice/ApplicationControllerAdvice.java
+++ src/main/java/apiBudget/apiBudget/controller/advice/ApplicationControllerAdvice.java
@@ -12,17 +12,17 @@ import apiBudget.apiBudget.dto.ErrorEntity;
 @ControllerAdvice
 public class ApplicationControllerAdvice {
 
-    // Gestionnaire pour DateTimeParseException (autres cas de format de date incorrect)
+    // Gestionnaire pour DateTimeParseException 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({ DateTimeParseException.class })
     public @ResponseBody ErrorEntity handleDateTimeParseException(DateTimeParseException exception) {
         String errorMessage = exception.getMessage();
 
-        // Vérifier si l'erreur est due à un DayOfMonth invalide
+        // Vérifier si l'erreur est due à un jour invalide
         if (errorMessage.contains("Invalid value for DayOfMonth")) {
             errorMessage = "Date de dépenses invalide. Le jour ne doit pas dépasser 31.";
         }
-        // Vérifier si l'erreur est due à un Month invalide
+        // Vérifier si l'erreur est due à un mois invalide
         else if (errorMessage.contains("Invalid value for MonthOfYear")) {
             errorMessage = "Date de dépenses invalide. Le mois ne doit pas dépasser 12.";
         } else {
