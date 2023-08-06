871cd84e51cb0b74f32f0bc8900ea4c218258df8
ayaya <ayaya6436@gmail.com>
2023-08-06 01:49:37 +0000

    modification message execption

--- src/main/java/apiBudget/apiBudget/controller/advice/ApplicationControllerAdvice.java
+++ src/main/java/apiBudget/apiBudget/controller/advice/ApplicationControllerAdvice.java
@@ -1,38 +1,29 @@
 package apiBudget.apiBudget.controller.advice;
-
-import java.time.format.DateTimeParseException;
-
 import org.springframework.http.HttpStatus;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.format.DateTimeParseException;
+
 import apiBudget.apiBudget.dto.ErrorEntity;
-import jakarta.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class ApplicationControllerAdvice {
-    // Gestionnaire pour EntityNotFoundException
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({ EntityNotFoundException.class })
-    public @ResponseBody ErrorEntity handleEntityNotFoundException(EntityNotFoundException exception) {
-        return new ErrorEntity(exception.getMessage());
-    }
 
-    // Gestionnaire pour DateTimeParseException
+    // Gestionnaire pour DateTimeParseException (autres cas de format de date incorrect)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({ DateTimeParseException.class })
     public @ResponseBody ErrorEntity handleDateTimeParseException(DateTimeParseException exception) {
         String errorMessage = exception.getMessage();
 
         // Vérifier si l'erreur est due à un DayOfMonth invalide
-        if (errorMessage.contains("Jour Invalide")) {
+        if (errorMessage.contains("Invalid value for DayOfMonth")) {
             errorMessage = "Date de dépenses invalide. Le jour ne doit pas dépasser 31.";
         }
         // Vérifier si l'erreur est due à un Month invalide
-        else if (errorMessage.contains("Mois Invalide")) {
+        else if (errorMessage.contains("Invalid value for MonthOfYear")) {
             errorMessage = "Date de dépenses invalide. Le mois ne doit pas dépasser 12.";
         } else {
             errorMessage = "Format de date invalide. Veuillez utiliser le format \"dd-MM-yyyy\".";
@@ -40,5 +31,4 @@ public class ApplicationControllerAdvice {
 
         return new ErrorEntity(errorMessage);
     }
-
 }
