8fe7065d278f709739abc8622119bff754a86340
ayaya <ayaya6436@gmail.com>
2023-08-09 11:14:29 +0000

    depenses

--- src/main/java/apiBudget/apiBudget/model/Depenses.java
+++ src/main/java/apiBudget/apiBudget/model/Depenses.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 import jakarta.persistence.Column;
