0a023fe58c970f540db537cf722ee3f2f1b608a2
Lancine223 <lacinekeita20@gmail.com>
2023-08-14 09:09:04 +0000

    My lats commit

--- src/main/java/apiBudget/apiBudget/repository/AlertCondiRepositor.java
+++
@@ -1,9 +0,0 @@
-package apiBudget.apiBudget.repository;
-
-import apiBudget.apiBudget.model.AlertConditionBoolean;
-import apiBudget.apiBudget.model.Alertes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertCondiRepositor extends JpaRepository<AlertConditionBoolean, Long> {
-
-}
